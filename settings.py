b4fe639c0f76ffcaa9c35469f1e7c2aed53719a1
KenMwaura1 <kemwaura@gmail.com>
2020-03-20 12:37:00 +0300

    created relevant tables

--- settings.py
+++ settings.py
@@ -3,9 +3,8 @@ from sqlalchemy import create_engine
 
 localdb = 'sqlite:////numbers.db'
 # sqlite_db = create_engine('sqlite:////numbers.db') # using absolute path
-engine = create_engine("mysql+pymysql://root:@localhost/test")
+engine = create_engine("mysql+pymysql://root:@localhost/test", echo=True)
 metadata = db.MetaData(bind=engine)
 engine.connect()
-metadata.create_all()
 
 print(engine.connect())
--- sort.py
+++ sort.py
@@ -1,6 +1,22 @@
 import numpy as np
 import timeit
 
+from sqlalchemy import MetaData, Table, Column, DateTime, Boolean, Integer,DECIMAL
+from datetime import datetime
+
+from settings import engine, metadata
+#metadata = MetaData()
+numbers = Table('sort', metadata,
+                Column('id', Integer(), primary_key=True),
+                Column('numbers', Integer()),
+                Column('created_on', DateTime(), default=datetime.now),
+                )
+runtime = Table('runtime', metadata,
+                Column('id', Integer(), primary_key=True),
+                Column('size_of_array', Integer()),
+                Column('time', DECIMAL()))
+
+metadata.create_all()
 tic = timeit.default_timer()
 size = 15
 random_list = np.random.randint(10, 1000, size)
@@ -12,5 +28,8 @@ ordered_random_list = np.sort(random_list)
 print(ordered_random_list)
 
 toc = timeit.default_timer()
+
+
 print(f'The processes took {toc-tic} seconds to complete')
+print(metadata)
 
