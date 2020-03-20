634fd0057e1a3d5916476d6a1fad84b39e58c589
KenMwaura1 <kemwaura@gmail.com>
2020-03-20 13:05:37 +0300

    Added sql insert code

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 localdb = 'sqlite:////numbers.db'
 # sqlite_db = create_engine('sqlite:////numbers.db') # using absolute path
 engine = create_engine("mysql+pymysql://root:@localhost/test", echo=True)
-metadata = db.MetaData(bind=engine)
+metadata = db.MetaData(engine.connect())
 engine.connect()
 
-print(engine.connect())
+print(metadata)
--- sort.py
+++ sort.py
@@ -1,20 +1,26 @@
 import numpy as np
 import timeit
 
-from sqlalchemy import MetaData, Table, Column, DateTime, Boolean, Integer,DECIMAL
+from sqlalchemy import MetaData, Table, Column, DateTime, Boolean, Integer, DECIMAL, insert
 from datetime import datetime
 
 from settings import engine, metadata
-#metadata = MetaData()
+
+# metadata = MetaData()
+
+conn = engine.connect()
+
 numbers = Table('sort', metadata,
                 Column('id', Integer(), primary_key=True),
                 Column('numbers', Integer()),
                 Column('created_on', DateTime(), default=datetime.now),
-                )
+                autoload=True)
 runtime = Table('runtime', metadata,
                 Column('id', Integer(), primary_key=True),
                 Column('size_of_array', Integer()),
-                Column('time', DECIMAL()))
+                Column('time', DECIMAL()),
+                #Column('created_on', DateTime(), default=datetime.now),
+                autoload=True)
 
 metadata.create_all()
 tic = timeit.default_timer()
@@ -29,7 +35,14 @@ print(ordered_random_list)
 
 toc = timeit.default_timer()
 
+duration = round(toc - tic, 5)
 
-print(f'The processes took {toc-tic} seconds to complete')
-print(metadata)
+ins = insert(runtime)
 
+r = conn.execute(ins,
+                 size_of_array=size,
+                 time=duration,
+                 created_on=datetime.now(),
+                 )
+print(r.inserted_primary_key)
+print(f'The processes took {duration} seconds to complete')
